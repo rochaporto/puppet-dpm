b5d3a87ef79e12655c9d148a910e65be885dd894
root <root@vmdm0006.cern.ch>
2011-04-27 13:38:54 +0200

    Added a template for a DPM build machine.

--- manifests/build.pp
+++ manifests/build.pp
@@ -1,8 +1,17 @@
+# Setup a machine with all the requirements to properly build lcgdm. 
+# 
+# == Examples
+#
+#
+# == Authors
+#
+# CERN IT/GT/DMS <it-dep-gt-dms@cern.ch>
+#
 class dpm::build {
 
   package { 
     ["subversion", "globus-gss-assist-devel", "globus-gssapi-gsi-devel", "voms-devel", 
-     "mysql-devel", "python-devel", "gsoap-devel", "CGSI-gSOAP-devel"]:
+     "mysql-devel", "python-devel", "gsoap-devel", "CGSI-gSOAP-devel", "lcg-CA"]:
       ensure => present,
   }
 }
