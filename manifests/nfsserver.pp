b974e42988a5b2f79e86fa96f5d341b23581521e
root <root@vmdm0006.cern.ch>
2011-08-24 14:19:16 +0200

    Added template file for dpm-nfs sysconfig.

--- manifests/nfsserver.pp
+++ manifests/nfsserver.pp
@@ -28,13 +28,22 @@ class dpm::nfsserver {
       require => Package["dpm-nfs-server"];
   }
 
+  file {
+    "sysconfig-dpm-nfs":
+      name    => "/etc/sysconfig/dpm-nfs",
+      owner   => root,
+      group   => root,
+      mode    => 0644,
+      content => template("dpm/dpm-nfs-sysconfig.erb"),
+      require => Package["dpm-nfs-server"];
+  }
+
   service { "dpm-nfs":
     ensure     => running,
     enable     => true,
     hasrestart => true,
     subscribe  => File["conf-dpm-nfs"],
-    require    => [ Package["dpm-nfs-server"], File["conf-dpm-nfs"], ],
-    status     => "ps aux | grep ganesha", # TODO: remove this once the init script 'status' works
+    require    => [ Package["dpm-nfs-server"], File["conf-dpm-nfs"], File["sysconfig-dpm-nfs"], ],
   }
 
 }
